bc837714a34b3b4b9650d843c6e182943b3284eb
Kamal23052004 <itx.kamal.official@gmail.com>
2024-04-22 20:34:05 +0530

    writing utils

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
 path: './env'
@@ -8,3 +9,11 @@ path: './env'
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+})
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+    constructor(statusCode, data, message = "Sucess"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
